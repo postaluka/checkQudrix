1c1d730645c831924b737dce56b21606862d5651
Artem <postaluka07@gmail.com>
2024-01-15 20:26:32 +0200

    update src01

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 import Experience from './Experience/Experience'
-
 
 /**
  * Canvas
@@ -12,5 +13,26 @@ const canvas = document.querySelector('canvas.webgl')
  */
 
 const experience = new Experience(canvas)
+
+
+// let CONFIG = {}
+
+// fetch('CONFIG.json')
+//     .then((response) =>
+//     {
+//         return response.json()
+//     })
+//     .then((obj) =>
+//     {
+//         // console.log(obj);
+//         CONFIG = obj
+//         console.log(CONFIG);
+//     })
+
+// const module = await import('./CONFIG.json', {
+//     with: { type: "json" },
+// });
+// console.log(module.size['element-name']);
+
 
 
